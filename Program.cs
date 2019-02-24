2941479014b573576189df456e79aefbe9c70256
iremsonguler <47424728+iremsonguler@users.noreply.github.com>
2019-02-24 23:05:20 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover
+{
+
+    public class Plateau
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public Plateau(Int32 x, Int32 y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Harita Boyutu: 5X5");
+            Console.WriteLine("Mevcut Konum: ");
+            Console.WriteLine("0 0 N");
+            Rover firstRover = new Rover(0, 0, Rover.Direction.N, new Plateau(5, 5));
+            Console.WriteLine("Yönlendirme Yapınız.");
+            firstRover.Command(Console.ReadLine());
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("Yeni Konum: ");
+            firstRover.GetPosition();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Rover.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace MarsRover
+{
+    public class Rover
+    {
+        public int PositionX { get; set; }
+        public int PositionY { get; set; }
+        Direction RoverDirection { get; set; }
+        Plateau Plateau { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public enum Direction
+        {
+            N = 90,
+            E = 180,
+            S = 270,
+            W = 360
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="direction"></param>
+        /// <param name="plateau"></param>
+        public Rover(Int32 x, Int32 y, Direction direction, Plateau plateau)
+        {
+            PositionX = x;
+            PositionY = y;
+            RoverDirection = direction;
+            Plateau = plateau;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void Move()
+        {
+            if (RoverDirection == Direction.N && Plateau.Y > PositionY)
+            {
+                PositionY++;
+            }
+            else if (RoverDirection == Direction.E && Plateau.X > PositionX)
+            {
+                PositionX++;
+            }
+            else if (RoverDirection == Direction.S && PositionY > 0)
+            {
+                PositionY--;
+            }
+            else if (RoverDirection == Direction.W && PositionX > 0)
+            {
+                PositionX--;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="directionCode"></param>
+        private void ChangeDirection(Char directionCode)
+        {
+            if (directionCode == 'L')
+                RoverDirection = (RoverDirection - 90) < Direction.N ? Direction.W : RoverDirection - 90;
+            else if (directionCode == 'R')
+                RoverDirection = (RoverDirection + 90) > Direction.W ? Direction.N : RoverDirection + 90;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="commandStr"></param>
+        public void Command(string commandStr)
+        {
+            foreach (var command in commandStr)
+            {
+                if (command == 'L' || command == 'R')
+                    ChangeDirection(command);
+                else if (command == 'M')
+                    Move();
+            }
+        }
+
+        private void ChangeDirection(char command)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string GetPosition()
+        {
+            string printedRoverPosition = string.Format("{0} {1} {2}", PositionX, PositionY, RoverDirection);
+            Console.WriteLine(printedRoverPosition);
+            return printedRoverPosition;
+        }
+    }
+}
